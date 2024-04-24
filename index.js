0d4ea889e3b0ad59b69f3b400cbeb62bebb2d420
PagesGuisus <cuentabot135@gmail.com>
2024-04-23 23:28:06 -0500

    x

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ constructor({element, tiltEffect}) {
 
   this.element = element;
   this.container = this.element.querySelector(".container");
-  this.size = [300*2, 360*2];
+  this.size = [300, 360];
   [this.w, this.h] = this.size;
 
   this.tiltEffect = tiltEffect;
